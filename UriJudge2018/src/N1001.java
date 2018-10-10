0ae4096a97d8becd560160e5710b7bce506a6404
diegom3nd35 <diegom3nd35@gmail.com>
2018-10-10 15:59:48 -0300

    obrigatorio

---
+++ UriJudge2018/src/N1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class N1001 {
+
+        public static void main(String[] args) {
+            Scanner ler = new Scanner(System.in);
+            int a = ler.nextInt();
+            int b = ler.nextInt();
+            int x = a+b;
+            System.out.println("X = "+x);
+
+        }
+    }
+
+
