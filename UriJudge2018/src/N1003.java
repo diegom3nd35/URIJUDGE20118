dee565f1338cf8e04d124226b4b0f1d075efcfdb
Diego Mendes <33493260+diegom3nd35@users.noreply.github.com>
2018-10-11 17:49:44 -0300

    Add files via upload

---
+++ UriJudge2018/src/N1003.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class N1003 {
+    public static void main(String args[]){
+
+        int soma;
+
+        Scanner ler = new Scanner(System.in);
+
+        int A = ler.nextInt();
+        int B = ler.nextInt();
+
+        soma = A+B;
+
+        System.out.println(soma);
+
+
+
+    }
+}
+
